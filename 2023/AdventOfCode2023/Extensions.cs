e0aadc70ba141227607163f0077e8464faf056b3
Anton Wass <anton.wass@outlook.com>
2024-01-03 00:45:31 +0100

    Amend

--- 2023/AdventOfCode2023/Extensions.cs
+++ 2023/AdventOfCode2023/Extensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-namespace AdventOfCode2023
+﻿namespace AdventOfCode2023
 {
     internal static class Extensions
     {
---
+++ 2023/AdventOfCode2023/Solutions/Day04.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode2023.Misc;
+
+namespace AdventOfCode2023.Sample;
+
+internal class Day04(PuzzleInputs inputs) : Day(inputs, 4)
+{
+    public override string PartOne(string input)
+    {
+        return "";
+    }
+
+    public override string PartTwo(string input)
+    {
+        return "";
+    }
+}
